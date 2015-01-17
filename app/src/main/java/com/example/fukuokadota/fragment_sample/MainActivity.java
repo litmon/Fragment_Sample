fa6b6927f17057d00b5fdb846dbe4d3bb372ed92
Fukuo Kadota <app.fk91@gmail.com>
2015-01-17 16:16:07 +0900

    Create production branch

--- app/src/main/java/com/example/fukuokadota/fragment_sample/MainActivity.java
+++ app/src/main/java/com/example/fukuokadota/fragment_sample/MainActivity.java
@@ -14,6 +14,7 @@ import android.view.ViewGroup;
 import android.os.Build;
 
 
+// it's branch is the production branch
 
 public class MainActivity extends ActionBarActivity implements SplashFragment.OnFragmentInteractionListener{
 
